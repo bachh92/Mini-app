65ff960c5b752bda7fb4d9a8641efa68959e10af
bachir.benzaoui <bachir.benzaoui@epitech.eu>
2020-06-09 13:18:30 +0200

    Filter and research done + modal when you click in the detaille

--- src/component/Card/CardItem.js
+++ src/component/Card/CardItem.js
@@ -1,30 +1,54 @@
 import React, {Component} from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
 
 export default class CardItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: props.id,
-            product_name: props.product_name,
-            supplier: props.supplier,
-            quantity: props.quantity,
-            unit_cost: props.unit_cost
-        };
+            id: this.props.id,
+            lgShow: false
+        }
+        this.Example = this.Example.bind(this);
+    }
+
+    Example()  {
+        return(
+        <>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>{this.props.product_name}</Card.Title>
+                <Card.Text>{this.props.supplier}</Card.Text>
+                <Card.Text>{this.props.quantity}</Card.Text>
+                <Card.Text>{this.props.unit_cost}</Card.Text>
+                <Button variant="primary" onClick={() => this.setState({lgShow: true})}>Details</Button>
+            </Card.Body>
+        </Card>
+        <Modal size="lg" show={this.state.lgShow} onHide={() => this.setState({lgShow: false})} aria-labelledby="example-modal-sizes-title-lg">
+        <Modal.Header closeButton>
+        <Modal.Title id="example-modal-sizes-title-lg">
+            Détails
+        </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>{this.props.product_name}</Card.Title>
+                <Card.Text>{this.props.supplier}</Card.Text>
+                <Card.Text>{this.props.quantity}</Card.Text>
+                <Card.Text>{this.props.unit_cost}</Card.Text>
+            </Card.Body>
+        </Card>
+        </Modal.Body>
+        </Modal>
+        </>
+        );
     }
 
     render () {
-        return(
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>{this.state.product_name}</Card.Title>
-                    <Card.Text>{this.state.supplier}</Card.Text>
-                    <Card.Text>{this.state.quantity}</Card.Text>
-                    <Card.Text>{this.state.unit_cost}</Card.Text>
-                    <Button variant="primary">Details</Button>
-                </Card.Body>
-            </Card>
-        );
+        return this.Example();
     }
 };
--- src/route/Home/Home.js
+++ src/route/Home/Home.js
@@ -3,18 +3,44 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import ListCard from '../../component/ListCard/ListCard';
-import Badge from 'react-bootstrap/Badge';
 import '../../assets/nav.css';
+import data from '../../assets/dataset-example.json';
+import ListGroup from 'react-bootstrap/ListGroup';
+import CardItem from '../../component/Card/CardItem';
 
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            search: ""
+            search: "",
+            list: [],
+            increase: false,
+            decrease: false
         }
-        this.textInput = React.createRef(); 
+        this.search = this.search.bind(this);
+        this.textInput = React.createRef();
+        this.handleIChange = this.handleIChange.bind(this);
+        this.handleDChange = this.handleDChange.bind(this);
+        this.printlist = this.printlist.bind(this);
+    }
+
+    componentDidMount() {
+        const list = data.map((data) =>
+            <CardItem key={data._id.$oid} product_name={data.product_name} supplier={data.supplier} quantity={data.quantity} unit_cost={data.unit_cost} /> 
+        );
+        this.setState({
+            list: list
+        });
+    }
+
+    search(data, index) {
+        if (this.state.decrease === false && this.state.increase === false) {
+            if ( this.state.search !== "" && data.props.product_name.toLowerCase().indexOf(this.state.search.toLowerCase()) === -1 ) {
+                return null;
+            }
+            return  <CardItem key={index} product_name={data.props.product_name} supplier={data.props.supplier} quantity={data.props.quantity} unit_cost={data.props.unit_cost} /> 
+        }
     }
 
     onChange = () => {
@@ -23,11 +49,37 @@ export default class Home extends Component {
         });
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
+    handleIChange() {
+        this.setState({
+            list: this.state.list.sort(function (a, b) {
+                var tmp = a.props.unit_cost.replace('$', '');
+                var tmp2 = b.props.unit_cost.replace('$', '');
+                    return parseInt(tmp) - parseInt(tmp2)
+                })
+        });
+    }
+    
+    handleDChange() {
+         this.setState({
+            list: this.state.list.sort(function(a, b){
+                var tmp = a.props.unit_cost.replace('$', '');
+                var tmp2 = b.props.unit_cost.replace('$', '');
+                return parseInt(tmp2) - parseInt(tmp);
+            }),
+            decrease: true
+        });
+    }
+
+    printlist() {
+        this.setState({
+            increase: false,
+            decrease: false
+        });
+        return this.state.list;
     }
 
     render() {
+        let button;
         return (
             <body>
                 <div>
@@ -36,18 +88,22 @@ export default class Home extends Component {
                     <Nav className="mr-auto">
                     <div className="filter">
                         <a>Prix:</a>
-                        <Button className="button" variant="outline-success"  size="sm">Croissant</Button>
-                        <Button className="button" variant="outline-success"  size="sm">Décroissant</Button>
+                        <Button className="button" variant="outline-success" onClick={() => this.handleIChange()} size="sm">Croissant</Button>
+                        <Button className="button" variant="outline-success" onClick={() => this.handleDChange()} size="sm">Décroissant</Button>
                     </div>
                     </Nav>
-                    <Form inline onSubmit={this.handleSubmit}>
+                    <Form inline>
                         <Form.Control ref={this.textInput} type="text" placeholder="Search" className="mr-sm-2" onChange={this.onChange}/>
-                        <Button variant="outline-success" >Search</Button>
+                        <Button variant="outline-success">Search</Button>
                     </Form>
                 </Navbar>
                 </div>
-                <div><ListCard search={this.state.search}/></div>
-          </body>
+                <ListGroup horizontal>
+                    {
+                          (this.state.decrease === false && this.state.increase === false) ? (this.state.list.map((data, index) => { return  this.search(data, index) })) : this.printlist()
+                    }
+                </ListGroup>          
+                </body>
         );
     }
 };
