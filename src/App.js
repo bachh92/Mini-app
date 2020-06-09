4cfae812735a50202cb45eadc4de588f741a55a8
bachir.benzaoui <bachir.benzaoui@epitech.eu>
2020-06-09 15:23:45 +0200

    [ADD] second page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import  { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from './route/Home/Home';
+import Details from './route/Details/Details';
 
 export default function App() {
   return (
@@ -10,6 +11,9 @@ export default function App() {
           <Switch>
             <Route path="/">
               <Home />
+            </Route>
+            <Route path="/details">
+              <Details />
             </Route>
           </Switch>
   </Router>
